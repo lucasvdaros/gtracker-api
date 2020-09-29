89927cf31aa832f1d3aaa64ce7efa6ae58f10cda
lucasvdaros <lucasdaros.eng@gmail.com>
2020-09-29 11:48:02 -0300

    project: Change scripts to mysql

--- sql/database.sql
+++ sql/database.sql
@@ -1,14 +1,5 @@
-USE [master]
-GO
-CREATE DATABASE [gtracker-db]
-GO
+CREATE DATABASE `gtracker-db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE [gtracker-db]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE Friend (
 	id int PRIMARY KEY IDENTITY(1,1),
 	name varchar(50) NOT NULL,
@@ -16,70 +7,49 @@ CREATE TABLE Friend (
 	email varchar(50) NULL
 )
 
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+CREATE TABLE `Friend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE Game(
-	id int PRIMARY KEY IDENTITY(1,1),
-	name varchar(50) NOT NULL,
-	dt_acquisition DATETIME NOT NULL,
-	kind int NOT NULL,
-	observation varchar(250) NULL
-	)
+CREATE TABLE `Game` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dt_acquisition` datetime NOT NULL,
+  `kind` int(11) NOT NULL,
+  `observation` varchar(250) COLLATE utf8_unicode_ci NOT NULL,  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+CREATE TABLE `SystemUser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `active`tinyint NOT NULL,
+  `login` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE SystemUser(
-	id int PRIMARY KEY IDENTITY(1,1),
-	name varchar(50) NOT NULL,
-	active bit NOT NULL,
-	login varchar(50) NOT NULL,
-	password nvarchar(75) NOT NULL	 
-)
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-USE [master]
-GO
-ALTER DATABASE [Equinox] SET  READ_WRITE 
-GO
+CREATE TABLE `Loan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `friend_id` int(11) NOT NULL,
+  `dt_start` datetime NOT NULL,
+  `observation` varchar(250) COLLATE utf8_unicode_ci NOT NULL,  
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_loan_friend` FOREIGN KEY (`friend_id`) REFERENCES `Friend` (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE Loan(
-	id int PRIMARY KEY IDENTITY(1,1),
-	friend_id int NOT NULL,
-	dt_start DATETIME NOT NULL,
-	observation varchar(250) NULL,
-	FOREIGN KEY (friend_id) REFERENCES Friend (id)
-);
+CREATE TABLE `LoanGame` (  
+  `loan_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `loan_status` int(11) NOT NULL,
+  `dt_end` datetime NULL,    
+  UNIQUE KEY `index_loan_id_on_game_id` (`loan_id`,`game_id`),
+  CONSTRAINT `fk_loan_lg` FOREIGN KEY (`friend_id`) REFERENCES `Friend` (`id`),
+  CONSTRAINT `fk_game_lg` FOREIGN KEY (`game_id`) REFERENCES `Game` (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE LoanGame(	
-	loan_id int NOT NULL,
-	game_id int NOT NULL,
-	loan_status int NOT NULL,
-	dt_end DATETIME NULL,
-	PRIMARY KEY (loan_id, game_id),
-	FOREIGN KEY (loan_id) REFERENCES Loan (id),
-	FOREIGN KEY (game_id) REFERENCES Game (id)		
-);
-
-USE [master]
-GO
-ALTER DATABASE [gtracker-db] SET  READ_WRITE 
-GO
-USE [gtracker-db]
-GO
-SET IDENTITY_INSERT [dbo].[SystemUser] ON 
-GO
-INSERT [dbo].[SystemUser] ([id], [name], [active], [login], [password]) VALUES (1, N'ADMIN', 1, N'admin', N'10000.i0wt/ndvvw0/T/Kop2S99A==.Uv33Y1sHv+QO05qdqvbKEg3A9pGkQqhvTiB3BRf69BA=')
-GO
-SET IDENTITY_INSERT [dbo].[SystemUser] OFF
-GO
+INSERT INTO `SystemUser` (`id`, `name`, `active`, `login`, `password`) VALUES (1,'ADMIN', 1, 'admin', N'10000.i0wt/ndvvw0/T/Kop2S99A==.Uv33Y1sHv+QO05qdqvbKEg3A9pGkQqhvTiB3BRf69BA=')
